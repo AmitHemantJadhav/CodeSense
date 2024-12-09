fd09a07813e8f698045bc3470fb11d52296f86cb
Amit Jadhav <amitkempten2008@gmail.com>
2024-12-09 04:14:22 -0500

    Add User model and update environment configuration
    - Introduced User model in Prisma schema with fields for id, createdAt, updatedAt, imageUrl, firstName, lastName, emailAddress, and credits.
    - Updated .gitignore to exclude local environment files.
    - Created initial migration for User model.
    - Added SyncUser page for user synchronization with Clerk.
    - Simplified Home page component.

---
+++ src/app/sync-user/page.tsx
@@ -0,0 +1,29 @@
+import { auth, clerkClient } from '@clerk/nextjs/server';
+import { notFound } from 'next/navigation';
+import React from 'react'
+
+const SyncUser = async () => {
+    const { userId} = await auth();
+
+    if(!userId) {
+        throw new Error('No user ID');
+    }
+    const client = await clerkClient();
+    const user = await client.users.getUser(userId);
+
+    if(!user.emailAddresses[0]?.emailAddress) {
+        return notFound();
+    }
+
+    await db.user.upsert{
+      //continue here
+    }
+
+
+
+  return (
+    <div>Hello</div>
+  )
+}
+
+export default SyncUser
